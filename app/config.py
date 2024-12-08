21c8ab3b5ff72807448a3067ef0dc14b922a1a78
Ricardo Oliveira <ricvenicius@gmail.com>
2024-12-08 17:13:34 -0300

    update cors port

--- app/config.py
+++ app/config.py
@@ -8,8 +8,8 @@ from fastapi.middleware.cors import CORSMiddleware
 def cors_middleware(api):
     api.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5500",  # BackEnd
-                       "http://127.0.0.1:5500"],  # FrontEnd
+        allow_origins=["http://localhost:5000",  # BackEnd
+                       "http://127.0.0.1:5000"],  # FrontEnd
         allow_credentials=True,
     )
 
